f83a68bb7acf53f4aaee68b2be7ba996dcce3548
Scott Baldwin <sbaldwin@scottbaldwin.com.au>
2025-06-27 21:20:27 +1000

    Current neovim setup
    - basic Lazy configuratoin
    - plugins using so far

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # UsefulStuff
 A dumping ground for stuff I find useful... git configs, powershell profiles etc...
+
+## Folders
+
+### neovim
+
+My NeoVim configuration
---
+++ neovim/init.lua
@@ -0,0 +1,19 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+require("config.lazy")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+local tsconfig = require("nvim-treesitter.configs")
+tsconfig.setup({
+  ensure_installed = {"lua", "c_sharp", "markdown"},
+  highlighting = { enable = true },
+  indent = { enable = true },
+})
